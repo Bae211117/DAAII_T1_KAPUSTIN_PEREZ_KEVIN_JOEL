cb0d4fee1ac203602e612b269d3a84b56786bda0
Bae211117 <i202122429@cibertec.edu.pe>
2024-06-07 21:29:22 -0500

    first commit

---
+++ src/test/java/pe/edu/cibertec/DAAII_T1_KAPUSTIN_PEREZ_KEVIN_JOEL/DaaiiT1KapustinPerezKevinJoelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_KAPUSTIN_PEREZ_KEVIN_JOEL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1KapustinPerezKevinJoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
